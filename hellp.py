a9ed1c621f8045cfd7a6e2281597b268ead27fa3
sumkai <39520789+sumkai@users.noreply.github.com>
2023-10-30 16:03:44 +0800

    Update hellp.py

--- hellp.py
+++ hellp.py
@@ -16,8 +16,8 @@ chrome_options.add_argument('--disable-gpu')
 # action  linux服务器驱动地址
 
 service = Service(executable_path="/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver")
-driver = webdriver.Chrome()  
-#driver = webdriver.Chrome(service=service,chrome_options=chrome_options)  
+#driver = webdriver.Chrome()  
+driver = webdriver.Chrome(service=service,chrome_options=chrome_options)  
 #driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver',chrome_options=chrome_options)    # Chrome浏览器  
 # driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver')    # Chrome浏览器  
 
