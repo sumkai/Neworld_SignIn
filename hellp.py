5edb6a430ab1ab9460679b0073ae71636f521d87
sumkai <39520789+sumkai@users.noreply.github.com>
2023-10-31 16:53:17 +0800

    Update hellp.py

--- hellp.py
+++ hellp.py
@@ -6,7 +6,6 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.chrome.service import Service
 from selenium.webdriver.common.by import By
 
-
 # 虚拟出Chrome界面
 chrome_options = Options()
 chrome_options = webdriver.ChromeOptions()
@@ -15,18 +14,9 @@ chrome_options.add_argument('--window-size=1420,1080')
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--disable-gpu')
 # action  linux服务器驱动地址
-
-#service = Service(executable_path="./chromedriver")
-#service = Service(executable_path="/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver")
 driver = webdriver.Chrome(options=chrome_options)  
-#driver = webdriver.Chrome(service=service,options=chrome_options)  
-#driver = webdriver.Chrome(service=service,chrome_options=chrome_options)  
-#driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver',chrome_options=chrome_options)    # Chrome浏览器  
-# driver = webdriver.Chrome(executable_path='/home/runner/work/Neworld_SignIn/Neworld_SignIn/driver/chromedriver')    # Chrome浏览器  
-
 # windows 系统驱动路径
 # driver = webdriver.Chrome(executable_path='D:\Downloads\chromedriver_win32\chromedriver.exe')    # Chrome浏览器
-
 
 # 环境变量中读取数据，包含账号密码，和登陆页面测试
 u = os.environ["USERNAME"]
@@ -40,33 +30,19 @@ time.sleep(5)
 # 账号密码登录版本
 driver.find_element(By.ID, value='email').clear()
 driver.find_element(By.ID, value="email").send_keys(u)
-
 driver.find_element(By.ID, value='passwd').clear()
 driver.find_element(By.ID, value="passwd").send_keys(p)
-
-
 #driver.find_element_by_id('email').clear()
 #driver.find_element_by_id("email").send_keys(u)
-
 #driver.find_element_by_id('passwd').clear()
 #driver.find_element_by_id("passwd").send_keys(p)
-
-
-
 time.sleep(1)
 driver.find_element(By.ID, value="login-dashboard").click()
-
 driver.refresh()#刷新页面 
-
-
 driver.refresh()#刷新页面 
-
-# time.sleep(5)
-
-# buttons = driver.find_element_by_xpath("//button[@id='checkin']")
-# print('buttons',buttons)
+time.sleep(5)
 
 #driver.find_element(By.ID, value="check-in").click() # 点击元素
 button=driver.find_element(By.ID, value="check-in") # 点击元素
-driver.execute_script("$(arguments[0]).click()",button)
+driver.execute_script("(arguments[0]).click()",button)
 print('ok')
